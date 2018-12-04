6655c68ac4eebe61383646f76a26cf28c99221fc
Michael Gaare <michael.gaare@gmail.com>
2018-12-04 18:04:41 -0500

    Update deps for kurosawa, clojure, aws
    Previously was using an unreleased version of kurosawa, the alpha
    clojure, and rather old aws sdk version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject org.purefn/egon "0.2.3-SNAPSHOT"
   :description "Interface to Amazon S3"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [com.taoensso/timbre "4.7.4"]
-                 [org.purefn/kurosawa "1.10.7"]
-                 [com.amazonaws/aws-java-sdk-core "1.11.142"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.142"]]
+                 [org.purefn/kurosawa.core "2.0.6"]
+                 [com.amazonaws/aws-java-sdk-core "1.11.462"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.462"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.stuartsierra/component "0.3.2"]]
                    :source-paths ["dev"]}})
