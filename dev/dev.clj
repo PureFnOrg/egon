04af122d7e645fbe9d0e42ab5f19eb7d2bf80009
Michieljoris <mail@axion5.net>
2021-04-29 11:50:27 +1000

    Add fetching versioned object

--- dev/dev.clj
+++ dev/dev.clj
@@ -2,21 +2,20 @@
   (:require
    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
    [com.stuartsierra.component :as component]
-   [org.purefn.egon.core :as s3]
+   [org.purefn.egon.core :as egon]
+   [org.purefn.kurosawa.k8s :as k8s]
+   [org.purefn.kurosawa.config :as config]
    [org.purefn.egon.api :as api]
    [org.purefn.kurosawa.log.core :as klog]
    [taoensso.timbre :as log]))
 
+(clojure.tools.namespace.repl/set-refresh-dirs "dev" "src" "test")
+
 (defn default-system
   []
   (component/system-map
-   :s3 (s3/s3 (merge (s3/default-config)
-                     {:cred
-                      
-                      {:access-key "",
-                       :secret-key ""}
-
-                      :buckets ["maxmind-db"]}))))
+   :s3 (egon/s3 (merge (egon/default-config)
+                       {:buckets ["test"]}))))
 
 (def system nil)
 
@@ -57,7 +56,7 @@
 (defn write-and-read-map []
   (let [s3 (:s3 system)
         m {:foo {:bar "baz"}}
-        bucket "maxmind-db"
+        bucket "test"
         key "test-map"]
     (api/store s3 bucket key m)
     (api/fetch s3 bucket key)))
@@ -65,8 +64,19 @@
 (defn write-and-read-map-with-meta []
   (let [s3 (:s3 system)
         m {:foo {:bar "baz"}}
-        bucket "maxmind-db"
+        bucket "test"
         key "test-map"
         meta {:version "2017"}]
     (api/store s3 bucket key m :meta meta)
     (api/fetch s3 bucket key :meta)))
+
+(defn write-and-read-versioned-object []
+  (let [s3 (:s3 system)
+        bucket "test"
+        key "my-key"
+        putObjectResult1 (api/store s3 bucket key {:v1 1})
+        putObjectResult2  (api/store s3 bucket key {:v2 2})
+        v1-id (.getVersionId putObjectResult1)
+        v2-id (.getVersionId putObjectResult2)]
+    {:v1 (api/fetch s3 bucket key :version-id v1-id :meta true)
+     :v2 (api/fetch s3 bucket key :version-id v2-id :meta true)}))
